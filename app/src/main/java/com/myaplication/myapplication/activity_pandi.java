4a6a3460fd9ef765e39a731dc7c699d403f05512
pandi-star <ekodann@gmail.com>
2020-09-21 23:21:02 +0700

    Hello World Capstone

---
+++ app/src/main/java/com/myaplication/myapplication/activity_pandi.java
@@ -0,0 +1,14 @@
+package com.myaplication.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_pandi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pandi);
+    }
+}
